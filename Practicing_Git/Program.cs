0ae5a24a9b861dcd1539e809aa3475c1aa4a8337
rlewisPersonal <lewisRjr@gmail.com>
2024-06-07 14:28:45 -0400

    changes to Program.cs

--- Practicing_Git/Program.cs
+++ Practicing_Git/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, this is Amanda adding something.");
+        System.Console.WriteLine("Reuben was here!");
     }
 }
 
