52905cb5fc40693c1e1571b269010154aae94d78
rlewisPersonal <lewisRjr@gmail.com>
2024-06-07 14:34:49 -0400

    Program.cs 2nd commit

--- Practicing_Git/Program.cs
+++ Practicing_Git/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
         Console.WriteLine("Hello, this is Amanda adding something.");
         System.Console.WriteLine("Reuben was here!");
+        System.Console.WriteLine("Reuben was here again - 2nd pull Request");
     }
 }
 
