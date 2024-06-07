78414f6e09b53094321d4af009f6f2cdc7ac0c81
kim-kitts <kitts.kim@gmail.com>
2024-06-07 14:31:25 -0400

    don't do it

--- Practicing_Git/Program.cs
+++ Practicing_Git/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, this is Amanda adding something.");
+        Console.WriteLine("You should not approve.....it will likely break something.");
     }
 }
 
